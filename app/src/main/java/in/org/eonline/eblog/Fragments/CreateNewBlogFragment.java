6c231e7ecac4f816b57d9f3c1d1a06ba2440d0db
aditdev <asktech6@gmail.com>
2018-09-16 22:43:53 +0530

    Added blog model, Insert for Blog Data in Firebase - 16th sept 2018

--- app/src/main/java/in/org/eonline/eblog/Fragments/CreateNewBlogFragment.java
+++ app/src/main/java/in/org/eonline/eblog/Fragments/CreateNewBlogFragment.java
@@ -1,18 +1,62 @@
 package in.org.eonline.eblog.Fragments;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import in.org.eonline.eblog.HomeActivity;
+import in.org.eonline.eblog.Models.BlogModel;
+import in.org.eonline.eblog.Models.UserModel;
 import in.org.eonline.eblog.R;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CreateNewBlogFragment extends Fragment {
+public class CreateNewBlogFragment extends Fragment  {
+    FirebaseFirestore db;
+    private EditText blogHeaderId;
+    private EditText blogContentId;
+    private EditText blogFooterId;
+    private AdView mAdView;
+    private EditText bannerAdIdEdit;
+    private Button submitBannerAdId;
+    private String bannerAdId;
+    private Spinner spinner;
+    private String item;
+    BlogModel blogmodel = new BlogModel();
+    UserModel userModel;
+    Map<String, String> blogMap = new HashMap<>();
 
 
     public CreateNewBlogFragment() {
@@ -23,8 +67,135 @@ public class CreateNewBlogFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_new_blog, container, false);
+
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initializeViews();
+        db = FirebaseFirestore.getInstance();
+
+        setSpinner();
+
+
+        submitBannerAdId.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                //  adMobUnitId = adMobAdUnitId.getText().toString();
+                mAdView = new AdView(getActivity());
+
+                bannerAdId = bannerAdIdEdit.getText().toString();
+                blogmodel.setBlogHeader(blogHeaderId.getText().toString());
+                blogmodel.setBlogContent(blogContentId.getText().toString());
+                blogmodel.setBlogFooter(blogFooterId.getText().toString());
+                blogmodel.setBlogLikes(0);
+                blogmodel.setBlogUser("Aditya Kamat");
+                blogMap.put("BlogHeader",blogmodel.getBlogHeader());
+                blogMap.put("BlogContent",blogmodel.getBlogContent());
+                blogMap.put("BlogFooter", blogmodel.getBlogFooter());
+                blogMap.put("BlogCategory",item);
+                blogMap.put("BlogUser", "Aditya Kamat");
+                blogMap.put("BlogLike", String.valueOf(blogmodel.getBlogLikes()));
+
+
+                addData();
+
+
+
+                mAdView.setAdSize(AdSize.BANNER);
+                mAdView.setAdUnitId(bannerAdId);
+               AdRequest adRequest = new AdRequest.Builder().addTestDevice("551B158596C8082704941DE131CEBAB9").build();
+               if
+                       (bannerAdId != null) mAdView.setVisibility(view.VISIBLE);
+                mAdView.loadAd(adRequest);
+
+            }
+
+        });
+    }
+
+    public void addData() {
+
+        db.collection("Blogs").document("Aditya").set(blogMap, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "DocumentSnapshot successfully written!");
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });}
+
+
+
+    public void initializeViews() {
+        blogHeaderId = (EditText) getView().findViewById(R.id.blog_header);
+        blogContentId = (EditText) getView().findViewById(R.id.blog_content);
+        blogFooterId = (EditText) getView().findViewById(R.id.blog_footer);
+        mAdView = (AdView) getView().findViewById(R.id.adView_user_ad);
+        submitBannerAdId = (Button) getView().findViewById(R.id.submitAdId);
+        bannerAdIdEdit = (EditText) getView().findViewById(R.id.adview_user_id);
+        spinner = (Spinner) getView().findViewById(R.id.spinner_category);
+
+
+    }
+
+    public void setSpinner() {
+        List<String> categories = new ArrayList<>();
+        categories.add("Travelling");
+        categories.add("Food");
+        categories.add("Cosmetics");
+        categories.add("Apparels");
+        categories.add("Technology");
+        categories.add("Cars and Bikes");
+        categories.add("Politics");
+        categories.add("Socialism");
+        categories.add("Bollywood and entertainment");
+        categories.add("Business");
+        categories.add("others");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                item = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
+
+
+    ;
 }
+
+
+
+
+
+
+
+
+
--- app/src/main/java/in/org/eonline/eblog/Models/BlogModel.java
+++ app/src/main/java/in/org/eonline/eblog/Models/BlogModel.java
@@ -2,6 +2,31 @@ package in.org.eonline.eblog.Models;
 
 public class BlogModel {
     public String blogHeader;
+    public String blogContent;
+    public String blogFooter;
+    public int blogLikes;
+    public String blogUser;
+
+
+    public String getBlogContent() {
+        return blogContent;
+    }
+
+    public void setBlogContent(String blogContent) {
+        this.blogContent = blogContent;
+    }
+
+
+
+    public String getBlogFooter() {
+        return blogFooter;
+    }
+
+    public void setBlogFooter(String blogFooter) {
+        this.blogFooter = blogFooter;
+    }
+
+
 
     public String getBlogHeader() {
         return blogHeader;
@@ -10,4 +35,22 @@ public class BlogModel {
     public void setBlogHeader(String blogHeader) {
         this.blogHeader = blogHeader;
     }
+
+    public int getBlogLikes() {
+        return blogLikes;
+    }
+
+    public void setBlogLikes(int blogLikes) {
+        this.blogLikes = blogLikes;
+    }
+
+
+    public String getBlogUser() {
+        return blogUser;
+    }
+
+    public void setBlogUser(String blogUser) {
+        this.blogUser = blogUser;
+    }
+
 }
