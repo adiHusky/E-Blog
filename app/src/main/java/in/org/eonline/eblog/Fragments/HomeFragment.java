1676d154b05f1e96016e06bbaf25a99e155371ff
aditdev <asktech6@gmail.com>
2018-09-21 00:10:57 +0530

    Retrieved User Data and Blog Data on Home Fragment - 21st-sept-2018

--- app/src/main/java/in/org/eonline/eblog/Fragments/HomeFragment.java
+++ app/src/main/java/in/org/eonline/eblog/Fragments/HomeFragment.java
@@ -2,10 +2,12 @@ package in.org.eonline.eblog.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,12 @@ import android.view.ViewGroup;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -25,12 +33,18 @@ import in.org.eonline.eblog.Models.BlogModel;
 import in.org.eonline.eblog.Models.UserModel;
 import in.org.eonline.eblog.R;
 
+import static android.content.ContentValues.TAG;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment implements UserAdapter.ClickListener, BlogAdapter.ClickListener {
 
-
+    FirebaseFirestore db;
+    private List<BlogModel> blogModelsList = new ArrayList<>();
+    private List<UserModel> userModelsList = new ArrayList<>();
+    BlogModel blogModel;
+    UserModel userModel;
     private AdView mAdView;
     private RecyclerView popularUsersRecyclerView, popularBlogsRecyclerView;
     private List<UserModel> userModels = new ArrayList<>();
@@ -53,11 +67,13 @@ public class HomeFragment extends Fragment implements UserAdapter.ClickListener,
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
 
         initializeViews();
+        setDataFirebase();
 
-        setPopularUsersRecyclerView();
-        setPopularBlogsRecyclerView();
+
+
 
         // Get the instance of Firebase storage
         storage = FirebaseStorage.getInstance();
@@ -78,21 +94,99 @@ public class HomeFragment extends Fragment implements UserAdapter.ClickListener,
         popularBlogsRecyclerView = (RecyclerView) getView().findViewById(R.id.popular_blogs);
     }
 
+    public void setDataFirebase(){
+        db.collection("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (document.exists()) {
+
+                                    setUserModel(document);
+
+                                };
+
+
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                            setPopularUsersRecyclerView();
+
+
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        db.collection("Blogs")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (document.exists()) {
+
+                                    setBlogModel(document);
+
+                                };
+
+
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                            setPopularBlogsRecyclerView();
+
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+    }
+
+    public void setUserModel(DocumentSnapshot document) {
+
+        userModel = new UserModel();
+        userModel.setUserFName(document.getString("UserFirstName"));
+
+        //userModel.setUserLName(document.getString("UserLastName"));
+
+       // userModel.setUserEmail(document.getString("UserEmailId"));
+
+        // blogModel.setBlogLikes(Integer.parseInt(document.getString("BlogLikes")));
+       // userModel.setUserContact(document.getString("UserContact"));
+        userModelsList.add(userModel);
+    }
+
+
+    private void setBlogModel(QueryDocumentSnapshot document) {
+
+        blogModel = new BlogModel();
+        blogModel.setBlogHeader(document.getString("BlogHeader"));
+        blogModel.setBlogFooter(document.getString("BlogFooter"));
+        blogModel.setBlogContent(document.getString("BlogContent"));
+        // blogModel.setBlogLikes(Integer.parseInt(document.getString("BlogLikes")));
+        blogModel.setBlogUser(document.getString("BlogUser"));
+        blogModel.setBlogCategory(document.getString("BlogCategory"));
+
+        blogModelsList.add(blogModel);
+        // setPopularBlogsRecyclerView();
+
+        // blogHeader = document.getString("BlogHeader");
+        Log.v(TAG, "DocumentSnapshot data: " + document.getData() );
+        // blogHeaderTextView.setText(blogModel.getBlogHeader());
+        // blogContentTextView.setText(blogModel.getBlogContent());
+        //blogFooterTextView.setText(blogModel.getBlogFooter());
+
+    }
+
     public void setPopularUsersRecyclerView() {
         popularUsersRecyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL, false);
         popularUsersRecyclerView.setLayoutManager(linearLayoutManager);
-
-        String userNames[]={"Vaibhav","Viraj","Aditya","Akshata",
-                "Aniket" };
-
-        for(int i = 0; i< userNames.length; i++) {
-            UserModel model = new UserModel();
-            model.setUserFName(userNames[i]);
-            userModels.add(model);
-        }
-
-        UserAdapter adapter = new UserAdapter(getActivity(), userModels, HomeFragment.this);
+        UserAdapter adapter = new UserAdapter(getActivity(),userModelsList , HomeFragment.this);
         popularUsersRecyclerView.setAdapter(adapter);
     }
 
@@ -100,17 +194,7 @@ public class HomeFragment extends Fragment implements UserAdapter.ClickListener,
         popularBlogsRecyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL, false);
         popularBlogsRecyclerView.setLayoutManager(linearLayoutManager);
-
-        String blogHeader[]={"Vaibhav","Viraj","Aditya","Akshata",
-                "Aniket" };
-
-        for(int i = 0; i< blogHeader.length; i++) {
-            BlogModel model = new BlogModel();
-            model.setBlogHeader(blogHeader[i]);
-            blogModels.add(model);
-        }
-
-        BlogAdapter adapter = new BlogAdapter(getActivity(), blogModels, HomeFragment.this);
+        BlogAdapter adapter = new BlogAdapter(getActivity(),blogModelsList , HomeFragment.this);
         popularBlogsRecyclerView.setAdapter(adapter);
     }
 }
